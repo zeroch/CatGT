956422682145400a450b653eadee127e0c7c8b2a
Ivan Jimenez <ijimenez3@gatech.edu>
2014-09-20 22:28:02 -0400

    Same as last rebase out

--- Server/Server.py
+++ Server/Server.py
@@ -1,5 +1,6 @@
 __author__ = 'ivan'
 
+
 import socket
 import sys
 from socket import error as socket_error
@@ -8,8 +9,8 @@ import command
 messages = command.send_jsons
 
 #create a TCP/IP Socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+sock_cat = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_owner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #bind the socket to the port
 #default server address else command line argument
 if not (len(sys.argv) == 1 or len(sys.argv) == 3) :
@@ -22,32 +23,34 @@ elif len(sys.argv) == 3:
     except:
         print "Invalid portnumber value. Make sure to input an integer"
         sys.exit(1)
+else:
+    server_address = ("localhost", 10000)
 
 print >> sys.stderr, 'Starting server on %s port %s' % server_address
-sock.bind(server_address)
-
+sock_cat.bind(server_address)
+server_address = (server_address[0], server_address[1]+1)
+sock_owner.bind(server_address)
+#callback
 #Listen for incoming connections
-sock.listen(1)
-
+sock_cat.listen(1)
+sock_owner.listen(1)
 
 while True:
     # Wait for a connection
     print >> sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
+    connection_cat, client_address_cat = sock_cat.accept()
+    connection_owner, client_address_owner = sock_owner.accept()
     try:
-        print  >> sys.stderr, 'connection from', client_address
-        index=0
+        print  >> sys.stderr, 'connection from', client_address_cat
         while True:
-            for message in messages:
-                print >>sys.stderr, 'sending %s' % message
-                connection.sendall(message)
-        else:
-            print >> sys.stderr, 'no more messages'
-            break
+            data = connection_owner.recv(35)
+            connection_cat.sendall(data)
     except socket_error as serr:
         #if we have a disconnect we try to reconnect
-        connection.close()
-        connection, client_address = sock.accept()
+        connection_cat.close()
+        connection_owner.close()
+        connection_cat, client_address_cat = sock_cat.accept()
+        connection_owner, client_address_owner = sock_owner.accept()
     finally:
         #clean up the connection
-        connection.close()
+        connection_cat.close()
