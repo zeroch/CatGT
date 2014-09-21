65165228628d3451aeccdabbdf7133011f1408e0
Ivan Jimenez <ijimenez3@gatech.edu>
2014-09-21 01:43:56 -0400

    [SERVER] Final Relay Server

--- Server/Server.py
+++ Server/Server.py
@@ -1,56 +1,40 @@
 __author__ = 'ivan'
 
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor
 
-import socket
-import sys
-from socket import error as socket_error
-import command
+latestCommand = "none"
 
-messages = command.send_jsons
+class MultiEcho(Protocol):
+   def _init_(self, factory):
+       self.factory = factory
 
-#create a TCP/IP Socket
-sock_cat = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock_owner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#bind the socket to the port
-#default server address else command line argument
-if not (len(sys.argv) == 1 or len(sys.argv) == 3) :
-    print "Invalid Number Of arguments use python Server.py 'ip.address' portNumber"
-    print len(sys.argv)
-    sys.exit(1)
-elif len(sys.argv) == 3:
-    try:
-        server_address = (sys.argv[1], int(sys.argv[2]))
-    except:
-        print "Invalid portnumber value. Make sure to input an integer"
-        sys.exit(1)
-else:
-    server_address = ("localhost", 10000)
+   def connectionMade(self):
+       self.factory.echoers.append(self)
 
-print >> sys.stderr, 'Starting server on %s port %s' % server_address
-sock_cat.bind(server_address)
-server_address = (server_address[0], server_address[1]+1)
-sock_owner.bind(server_address)
-#callback
-#Listen for incoming connections
-sock_cat.listen(1)
-sock_owner.listen(1)
+   def dataReceived(self, data):
+       self.update_data(data,self.factory.echoers)
 
-while True:
-    # Wait for a connection
-    print >> sys.stderr, 'waiting for a connection'
-    connection_cat, client_address_cat = sock_cat.accept()
-    connection_owner, client_address_owner = sock_owner.accept()
-    try:
-        print  >> sys.stderr, 'connection from', client_address_cat
-        while True:
-            data = connection_owner.recv(35)
-            connection_cat.sendall(data)
-    except socket_error as serr:
-        #if we have a disconnect we try to reconnect
-        connection_cat.close()
-        connection_owner.close()
-        connection_cat, client_address_cat = sock_cat.accept()
-        connection_owner, client_address_owner = sock_owner.accept()
-    finally:
-        #clean up the connection
-        connection_cat.close()
+   def connectionLost(self, reason):
+       self.factory.echoers.remove(self)
+
+   def update_data(self,data,echoers):
+       global latestCommand
+       global changed
+       if data !=latestCommand:
+           for echoer in echoers:
+               echoer.transport.write(data)
+           latestCommand = data
+
+
+
+class MultiEchoFactory(Factory):
+   def _init_(self):
+       self.echoers = []
+
+   def buildProtocol(self, addr):
+       return MultiEcho(self)
+
+reactor.listenTCP(4321, MultiEchoFactory())
+reactor.run()
+
--- Server/echo_client.py
+++ Server/echo_client.py
@@ -1,21 +1,4 @@
-import socket
-import sys
+from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.internet import reactor
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)  #(host,port)
-#server_address = ('198.74.55.55',10000) #(host,port)
-print >> sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
-
-try:
-    #while amount_received < amount_expected:
-    data = sock.recv(35)
-    while data:
-        print >> sys.stderr, 'received "%s"' % data
-        data = sock.recv(35)
-finally:
-    print >> sys.stderr, 'closing socket'
-    sock.close()
