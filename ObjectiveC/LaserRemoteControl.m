55c1da3ebbaf6f6fd238552402903a5e0d8de369
otherworldman <otherworldman@gmail.com>
2014-09-21 03:30:46 -0400

    more accurate control, by not sending the same direction.

--- ObjectiveC/LaserRemoteControl.m
+++ ObjectiveC/LaserRemoteControl.m
@@ -12,7 +12,7 @@
 
 #define DIRECTION_THRESHOLD_LEFT_RIGHT 10
 #define DIRECTION_THRESHOLD_UP_DOWN 10
-#define POWER_THREDSHOLD_ON_OFF 2
+#define POWER_THREDSHOLD_ON_OFF 30
 
 NSString *const directionUp = @"UP";
 NSString *const directionDown = @"DOWN";
@@ -37,7 +37,7 @@ static dispatch_queue_t serialQueue;
 @property(nonatomic)LaserGestureDirection direction;
 @property(nonatomic,strong)NSInputStream *inputStream;
 @property(nonatomic,strong)NSOutputStream *outputStream;
-
+@property(nonatomic)NSString *previousSend;
 @end
 
 @implementation LaserRemoteControl
@@ -128,7 +128,6 @@ static dispatch_queue_t serialQueue;
         
         dispatch_async(dispatch_get_main_queue(), ^{
             if (_updateGesture) {
-                NSLog(@"Updating Gesture %ld",self.direction);
                 //Do TCP Socket here.
                 [self sendGestureToServer:self.direction];
                 [self reset];
@@ -159,9 +158,13 @@ static dispatch_queue_t serialQueue;
         default:
             break;
     }
-    
+    if (response!=_previousSend) {
+
     NSData *data =[[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+        NSLog(@"%@",response);
+    _previousSend = response;
     [self.outputStream write:[data bytes] maxLength:[data length]];
+    }
 }
 
 -(void)sendPowerCommandToServer{
@@ -173,10 +176,16 @@ static dispatch_queue_t serialQueue;
         response = powerOff;
     }
     
-    NSData *data =[[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
-    [self.outputStream write:[data bytes] maxLength:[data length]];
     
-    [self reset];
+    if (response!=_previousSend) {
+        NSData *data =[[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+        NSLog(@"%@",response);
+        _previousSend = response;
+        
+        [self.outputStream write:[data bytes] maxLength:[data length]];
+        
+        [self reset];
+    }
 }
 
 -(void)updateOnAnOffLaser:(BOOL)toggle{
